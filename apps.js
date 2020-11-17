723c118cf0e2d2f35d39e265c5a4553cf1f299a2
Felix Atsu Kofi Ackuayi <felixackuayi@aol.com>
2020-11-17 15:55:32 +0000

    ADD product to the cart...set cart values...save cart in local storage....

--- apps.js
+++ apps.js
@@ -11,7 +11,10 @@ const productDoM =  document.querySelector('.products-center');/*9*/
 
 
 //10. CART: This will be our main cart.Where we will be placing information.Getting information from localStorage
-const cart =  [];
+let cart =  [];
+
+//buttons
+let buttonsDOM =[];
 
 //11. creating classes and methods. Will be responsible for getting the products locally.
 class Products{
@@ -96,8 +99,95 @@ class UI {
 
           productDoM.innerHTML=result;
       };
+  //After displaying the product let get the buttons
+    getBagButtons(){
+       /*spread operator used here*/
+        const buttons = [...document.querySelectorAll(".bag-btn")];
+
+        buttonsDOM = buttons;
+        /*console.log(btns);*/
+        /*now using the dataset to get ID of each item*/
+        buttons.forEach(button =>{
+            let id = button.dataset.id; //get the ID
+            //console.log(id);
+            /*cart is an array .Using the find method I will have on the arrays and find me that Item if it is in the cart. Whatever item that is in the cart
+            * get me a callback function and within the function I pass the argument with the item that if the item ID matches with the id that I have in buttons then something must happen
+            * If the item will be in cart then, I want to do two things
+            * */
+            let inCart = cart.find(item=> item.id === id);
+            if (inCart){
+                button.innerHTML = "Cart Captured";
+                button.disabled = true;
+            }else{
+
+                /*
+                * 1. we get Cart Item from products
+                * 2. We added that cart item through cart array that I had
+                * 3. Using the the Storage to save the cart in the storage
+                * */
+                button.addEventListener('click', (e) =>{
+                    e.target.innerText = "Cart Captured";
+                    e.target.disabled = true;
+                    //get product from products based on the ID
+                    let cartItem = {...Storage.getProduct(id), amount: 1}; //using destructor through spread operator.making the array in a method. asking spread spread operator to get all the properties and values.
+                    //console.log(cartItem)
 
 
+                    //ADD product to the cart
+                    cart = [...cart, cartItem ]
+                    //console.log(cart)
+
+                    //save cart in local storage
+                        Storage.saveCart(cart);
+                    //set cart values
+                    this.setCartValues(cart)
+                    //display cart items
+                    this.addCartItem(cartItem);
+                    //show the cart
+
+                })
+            }
+
+        })
+
+    }
+    setCartValues(cart){
+       let tempTotal = 0;
+       let itemsTotal =0;
+       cart.map(item=> {
+           tempTotal += item.price * item.amount;
+           itemsTotal += item.amount;
+       })
+        cartTotal.innerHTML = parseFloat(tempTotal.toFixed(2));
+       cartItems.innerHTML = itemsTotal;
+       //console.log(cartTotal,cartItems)
+
+    }
+
+    //getting item dynamically
+    addCartItem(item){
+        const div = document.createElement('div');
+        div.classList.add('cart-item');
+        div.innerHTML = `
+       
+         <!--<img src="./images/fruit1.jpg" alt="product">-->
+         <img src="${item.image}" alt="product">
+                                <div>
+                                    <!--<h4>Carrot</h4>-->
+                                    <h4>${item.title}</h4>
+                                    <!--<h5>GHC 50</h5>-->
+                                    <h5>GHC ${item.price}</h5>
+                                    <span class="remove-item">remove</span>
+                                </div>
+                                <div>
+                                    <i class="fas fa-chevron-up" style="font-size:24px;color:black"></i>
+                                    <p class="item-amount">1</p>
+                                    <i class="fas fa-chevron-down" style="font-size:24px;color:black"></i>
+
+                                </div>
+       
+       `;
+    }
 }
 
 //13. Actual localStorage Class
@@ -119,6 +209,17 @@ class Storage{
         localStorage.setItem("products", JSON.stringify(products));
     }
 
+
+      static getProduct(id){//id gotten from button
+
+        let products = JSON.parse(localStorage.getItem('products'));//will return an array from local storage
+          return products.find(product => product.id === id);
+
+      }
+
+      static saveCart(){
+        localStorage.setItem('cart', JSON.stringify(cart));
+      }
 }
 
 
@@ -145,6 +246,10 @@ document.addEventListener("DOMContentLoaded", () =>{
 
         ui.displayProducts(products)
         Storage.saveProducts(products);
+    }).then(()=> {//this means run the then() when i'm done running the display product
+
+        ui.getBagButtons();
+
     });
 
 
